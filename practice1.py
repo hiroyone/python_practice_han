620cbb32739ed5cd17dec41f50da80c2370a9bb6
Hiroki Yonetani <yonetani@team-lab.com>
2018-11-11 22:48:51 +0900

    First Python Practice file

---
+++ practice1.py
@@ -0,0 +1,101 @@
+print("Hello World!")
+
+apple = "apple"
+
+if 2 == 2:
+   print("True")
+else:
+   print("False")
+
+if 5 > 2:
+    print("Five is greater than two!")
+
+# This is a comment
+
+
+"""This is a 
+multiline docstring."""
+print("Hello, World!")
+
+
+"""
+I am writing Python code.
+This is multi-line comments in the middle
+"""
+x = 5
+if x == 5:
+    print(1000)
+y = "John"
+print(x)
+print(y)
+
+# Variable is hensu in Japanese
+
+# Variable declaration is not possible in Python
+# dynamic typed language
+x = 4  # x is of type int
+x = "Sally"  # x is now of type str
+print(x)
+
+# Output
+x = "awesome" #String
+print("Python is " + x)
+
+# String Concatenation
+x = "Python is "
+y = "awesome"
+z = x + y
+print(z)
+
+# add numbers
+x = 5
+y = 10
+print(x + y)
+
+# String and number cannnot be joined, so number should be converted to string
+x = "Python is "
+y = 5
+z = x + str(y)
+print(z)
+
+"""
+Review
+- Different Languages 
+    - Frontend, Clientside
+        - HTML 
+        - CSS (Bootstrap)
+        - JavaScript (ReactJS, VueJS ...)
+    - Backend, Serverside
+        - Java (Enterprise)
+        - C Family (Computation, Academics)
+        - Python, Ruby (General Purpose, Prototype, OS, A.I. Web Scraping, Education)
+        - Go Language (Compact and Fast, good for web services)
+        - PhP (for web service)
+    - Mobile
+        - Kotlin (Android)
+        - Swift (iOS)
+    - Terminal (Mac), Commmand Prompt (Windows)
+        - Bash (Mac)
+
+- Install: Official or Anaconda (Distribution Package)
+
+- Editor
+    - VS code <- recommended
+    - Jupyter Notebook <- Best for data science or A.I.
+
+- Execution
+    - Command Line (Execute file)
+    - interactive shell (inc. Jupyter Notebook)
+
+- Python features
+    - Python 2.0 series and 3.0 series (3.0 is strongly recommended)
+    - interpreted langauge (program is read each time) <-> compiled language
+    - interactive language (immediate feedback for each statement => for better debugging)
+    - dynamically typed language (no declaration, type can be changed in the middle) <-> Static Language (Java, C)
+    - Indentation
+    - Pythonic (Undestandable writing style, Zen princples) => Pythonista
+
+- Tutorial
+    - W3: https://www.w3schools.com/python/python_iterators.asp
+"""
+
